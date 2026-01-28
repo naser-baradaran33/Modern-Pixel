8ba99cf319fcb7e68348f1690e25c994c92ca20c
daniel <danielbaradaran31-hash>
2026-01-28 12:46:48 +0100

    update project information

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,14 +2,14 @@ const projects = [
     {
        
         title: "Portfolio Macbook",
-        description: "A personal portfolio website to showcase my projects and skills whit React.",
+        description: "💻 MacBook Portfolio A unique portfolio project that works like a virtual MacBook — complete with windows, a dock, a Finder, and interactive apps. Instead of a traditional portfolio website, you get an immersive desktop‑style experience.",
         image: "/images/Portfolio.jpg",
         live:"https://portfolio-wine-beta-64.vercel.app/",
         github:"https://github.com/danielbaradaran31-hash/Portfolio-Macbook"
       },
     {
         title: "E-commerce Store Magic Schuhe",  
-        description: "An online store built with React and Redux for state management.",
+        description: "Magic Shoes is a modern web project built with HTML, CSS, and JavaScript. It features a fully responsive layout, smooth animations, and high‑quality user interactions. The design includes elegant sliders, a modern navigation bar, dynamic UI components, and numerous animations that enhance the overall user experience. Every element has been carefully crafted to be both functional and visually appealing.",
         image: "/images/Magic-Schuhe.jpg",
         live:"https://magic-schuhe.vercel.app/",
         github:"https://github.com/danielbaradaran31-hash/Magic-Schuhe"
@@ -17,14 +17,14 @@ const projects = [
 
 
     { title: "E-commerce Store Sushi Kushi",  
-        description: "An online store built with React and Redux for state management.",
+        description: "This project is a modern, responsive, and animated landing page created for the fictional Japanese food brand “Sushiman.” Its minimalist design, clear user flow, and subtle animations make it a professional example of contemporary frontend development.",
         image: "/images/Suchi-Kushi.jpg",
         live:"https://sushi-english-vite.vercel.app/",
         github:"https://github.com/danielbaradaran31-hash/Sushi-Kushi",
     },
 
     { title: "E-commerce Store Star Cafe",  
-        description: "An online store built with React and Redux for state management.",
+        description: "A modern, elegant, and minimalist landing page designed to showcase the “Black Lifestyle” and premium coffee products. Built with React, Vite, TailwindCSS, and Framer Motion, the project focuses on high performance, smooth animations, and a professional user experience.",
         image: "/images/Star Cafe.jpg",
         live:"https://star-cafe-jet.vercel.app/",
         github:"https://github.com/danielbaradaran31-hash/Star-Cafe",
