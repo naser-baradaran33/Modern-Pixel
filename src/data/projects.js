bdb482be980702fa0811c61f05166701462aa868
daniel <danielbaradaran31-hash>
2026-02-15 20:23:57 +0100

    add projekte

--- src/data/projects.js
+++ src/data/projects.js
@@ -44,7 +44,7 @@ const projects = [
     },
 
     { title: "Admin-Dashboard",  
-        description: "Admin-Dashboard mit React+TypeScript+Vite",
+        description: "Admin-Dashboard mit React+TypeScript+Vite ⭐ Allgemeine Seitenstruktur Sidebar mit minimalistischen Icons, Navbar mit Suchleiste und Benachrichtigungen, Stat Cards in Grün/Rot, Chart‑Bereich mit ApexCharts, abelle der neuesten Bestellungen, User‑Activity‑Widget   ",
         image: "/images/Dashboard.png",
         live:"https://dashboard-psi-five-82.vercel.app/",
         github:"https://github.com/danielbaradaran31-hash/Iphon_gsap_app",
