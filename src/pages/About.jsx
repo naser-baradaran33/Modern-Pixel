a88d8d90ab3880d29433dffee255215dd8942af3
naser-baradaran33 <danielbaradaran31@gmail.com>
2026-03-12 18:54:59 +0100

    add skill

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,8 +6,9 @@ function About() {
       <section className="py-20 px-6 bg-gray-100 dark:bg-gray-800 dark:text-white text-center">
         <h1 className="text-4xl font-bold mb-6">About Me</h1>
         <p className="max-w-2xl mx-auto text-lg text-gray-700 dark:text-gray-300">
-          I'm a passionate React and JavaScript developer who loves building
-          interactive and modern web applications.
+          Frontend developer specializing in React, Next.js, and TypeScript.
+        I build modern, scalable web applications with clean UI architecture and strong focus on performance and maintainability.
+          I also have experience building full-stack features using Next.js including authentication and API routes.
         </p>
       </section>
 
